402f867d4fccfa5f1b4a59ba33817d05c8ba1f90
Eric Xv <601198602@qq.com>
2020-11-05 10:41:43 +0800

    建立项目

---
+++ src/main/java/com/leo/ManevtestApplication.java
@@ -0,0 +1,13 @@
+package com.leo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManevtestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManevtestApplication.class, args);
+    }
+
+}
